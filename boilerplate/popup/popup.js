fb2ede622e1d1d252cd4177fa2d96e2bf7865e24
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-03-02 20:08:06 -0800

    popup html page + communication to content.js

--- boilerplate/popup/popup.js
+++ boilerplate/popup/popup.js
@@ -1,1 +1,19 @@
-console.log('Popup.html is connected to popup.js...')
+console.log("popup.html is connected to popup.js...");
+
+const userInput = document.getElementById("userInput");
+
+const changeText = event => {
+  const params = {
+    active: true,
+    currentWindow: true
+  };
+  const gotTabs = tabs => {
+    let message = {
+      text: event.target.value
+    };
+    chrome.tabs.sendMessage(tabs[0].id, message);
+  };
+  chrome.tabs.query(params, gotTabs);
+};
+
+userInput.addEventListener("input", changeText);
