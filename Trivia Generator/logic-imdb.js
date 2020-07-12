dade21e092c2917ec52b102a311e9ef60bccfb6b
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-07-11 21:49:52 -0700

    imdb front page complete

--- Trivia Generator/logic-imdb.js
+++ Trivia Generator/logic-imdb.js
@@ -1,13 +1,6 @@
-// // rating test
-// var userRating = document.querySelector('div.ratings_wrapper').innerText.split(/\s/);
-// var url = window.location.href.match(/https:\/\/www.imdb.com\/title\/tt+\d{1,}/)[0];
-// var ratingUrl = `${url}/ratings?ref_=tt_ov_rt`;
-
 // console.log(`What was the IDMB users rating for ${title}\t${userRating[1]} IMDB users have given a weighted average vote of ${userRating[0].split("/").join(" / ")} for ${title}\t${ratingUrl}`);
 
-clear()
-
-var genres = [
+var imdbGenres = [
   "Action",
   "Animation",
   "Biography",
@@ -37,10 +30,11 @@ var genres = [
   "Reality-TV",
   "Talk Show",
   "Reality TV",
-  "Sports",
+  "Short",
+  "Sports"
 ];
 var imdb = {};
-var capturePoint, el, captureData;
+var capturePoint, el, capturedData;
 
 // Capture Plot, Director, Writers, Stars
 capturePoint = document.querySelector("div.summary_text");
@@ -50,6 +44,7 @@ while (el !== null) {
   capturedData.push(el.innerText);
   el = el.nextElementSibling;
 }
+
 capturedData.forEach((data, i) => {
   var format = {
     0: `Plot: <DO NOT USE THIS VALUE>`,
@@ -63,13 +58,12 @@ capturedData.forEach((data, i) => {
 });
 
 // Capture Title, Rating, Time, Genre, Release
-captureData = document
+capturedData = document
   .querySelector("div.title_wrapper")
   .innerText.split(" | ");
-captureData.forEach((data, i) => {
+capturedData.forEach((data, i) => {
   var format = {
     RuntimeHM: `RuntimeHM: ${data}:`,
-    Genre: `Genre: ${data}`,
     "Release Date": `Release Date: ${data}`,
   };
   var temp;
@@ -84,7 +78,11 @@ captureData.forEach((data, i) => {
         var [title, originalTitle, Rating] = data.split(/\n/);
         imdb['Title'] = [title]
         imdb['Original Title'] = [originalTitle.split(" (")[0]]
-        imdb['Rating'] = [Rating]
+        if (imdb.hasOwnProperty('Motion Picture Rating (MPAA)')) {
+          imdb['Motion Picture Rating (MPAA)'] = {...imdb['Motion Picture Rating(MPAA)'], "Short": [Rating]}
+        } else {
+          imdb['Motion Picture Rating (MPAA)'] = {"Short": [Rating]}
+        }
       }
     } else {
       if(!!data.match(/\d{1,}h/) || !!data.match(/\d{1,}min/)) {
@@ -94,7 +92,11 @@ captureData.forEach((data, i) => {
       } else if (!!data.match(/\nG|PG|PG-13|R|NC-17/)) {
         var [title, Rating] = data.split(/\n/);
         imdb['Title'] = [title]
-        imdb['Rating'] = [Rating]
+        if (imdb.hasOwnProperty('Motion Picture Rating (MPAA)')) {
+          imdb['Motion Picture Rating (MPAA)'] = {...imdb['Motion Picture Rating(MPAA)'], "Short": [Rating]}
+        } else {
+          imdb['Motion Picture Rating (MPAA)'] = {"Short": [Rating]}
+        }
       }
     }
     
@@ -105,7 +107,20 @@ captureData.forEach((data, i) => {
       temp = `RuntimeHM: ${runtime}`;
       imdb['Title'] = [title.split(/:\s{0,1}/)[1]];
     }
-  } else if (data.includes(",") || genres.includes(data)) {
+  } else if (data.includes(",")) {
+    var genres = data.split(", ");
+    genres.forEach(genre => {
+      if(imdbGenres.includes(genre)) {
+        if (imdb.hasOwnProperty['Genre']) {
+          if (!imdb['Genre'].includes(genre)) {
+            imdb['Genre'].push(genre)
+          }
+        }
+        imdb['Genre'] = [genre];
+      } else {
+        console.log(`${genre} should be added to the Known IMDb Genres List!`)
+      }
+    })
     temp = format["Genre"];
   } else if (data.includes("(")) {
     temp = format["Release Date"];
@@ -114,72 +129,93 @@ captureData.forEach((data, i) => {
   if (key && value) { imdb[key] = [value]}
 });
 
-console.log(imdb);
-
-
-
-
-
-
-
-// rows = rows.concat(second.shift().split(/\n/).concat(second))
-
 // IMDb User Rating
-var third = document
-  .querySelector("div.ratings_wrapper")
-  .innerText.split(/\s/)
-  .slice(0, 2);
-var [rating, users] = third;
-rows = rows.concat([`Rating: ${rating.replace("/", " / ")}`]);
+capturedData = document.querySelector("div.ratings_wrapper").innerText.split(/\n/).slice(0,2);
+var [rating, users] = capturedData;
+if (rating && users) {
+  imdb['Rating'] = {
+    "Average Rating": rating.replace("/", " / "),
+    "No. of Users": users
+  }
+}
 
-// MPAA Rating
-var fourth = document.querySelector("#titleStoryLine > h2");
-var sBlock = [];
-el = fourth;
+// MPAA Rating, Expanded Genre, and Storyline
+capturePoint = document.querySelector("#titleStoryLine > h2");
+capturedData = [];
+el = capturePoint;
 while (el !== null) {
-  sBlock.push(el);
+  capturedData.push(el.innerText);
   el = el.nextElementSibling;
 }
-sBlock = sBlock.map((x) => x.innerText);
-sBlock = sBlock
-  .filter((x) => x && x.includes("MPAA"))[0]
-  .split(" | ")[0]
-  .replace(/\n/, ": ");
-rows = rows.concat(sBlock);
+
+capturedData.forEach((data, i) => {
+  if(data.includes('MPAA')) {
+    var [key, value] = data.split(/\n/);
+    if (imdb.hasOwnProperty(key)) {
+      imdb[key] = {...imdb[key], "Detailed": [value.split(" | ")[0].replace("Rated ", "")]}
+    } else {
+      imdb[key] = {"Detailed": [value.split(" | ")[0].replace("Rated ", "")]}
+    }
+  }
+  if(data.includes('Storyline')) {
+    imdb['Storyline'] = [capturedData[i+1].split(' Written by')[0]]
+  }
+  if(data.includes('Genres')) {
+    var genres = data.split(/:\s{0,}/)[1].split(" | ");
+    genres.forEach(genre => {
+      if(imdbGenres.includes(genre)) {
+        if (imdb.hasOwnProperty['Genre']) {
+          if (!imdb['Genre'].includes(genre)) {
+            imdb['Genre'].push(genre)
+          }
+        }
+        imdb['Genre'] = [genre];
+      } else {
+        console.log(`${genre} should be added to the Known IMDb Genres List!`)
+      }
+    })
+  }
+})
 
 // Capture Country, Language, Release Date, AKA, Filming Locations, Budget, Opening Weekend, Gross, Cumulative Worldwide Gross, Production Co, Runtime, Sound Mix, Color, Aspect Ratio
-var fifth = document.querySelector("#titleDetails > h2");
-var dBlock = [];
-el = fifth;
+capturePoint = document.querySelector("#titleDetails > h2");
+el = capturePoint;
+capturedData = [];
 while (el !== null) {
-  dBlock.push(el);
+  capturedData.push(el.innerText);
   el = el.nextElementSibling;
 }
-dBlock = dBlock.map((x) => x.innerText);
-dBlock = dBlock
-  .filter((x) => {
-    var exclude = [
-      "Details",
-      "Edit",
-      "Box Office",
-      "Company Credits",
-      "Technical Specs",
-      "full technical specs",
-    ];
-    return (
-      x &&
-      !x.includes("Official Sites:") &&
-      !x.includes("more on IMDbPro") &&
-      !exclude.includes(x)
-    );
-  })
-  .map((x) =>
-    x
-      .replace(/(:\s{0,})/g, ": ")
-      .split("See more")[0]
-      .trim()
-  );
 
-rows = rows.concat(dBlock);
+capturedData.forEach((data, i) => {
+  if (data.includes(":") && !data.includes('Official Sites')) {
+    if(data.includes('Aspect Ratio')) {
+      var [key, value] = ["Aspect Ratio", data.split('Aspect Ratio: ')[1]]
+      imdb[key] = [value]
+    } else {
+      var [key, value] = data.split(/:\s{0,}/)
+      value = [value.split(/ See full summary| See more/)[0]];
+      if (!!value[0].match(/\, | \| /) && !key.includes('Filming Locations')) {
+        value = value[0].split(/, | \| /)
+      }
+      if (key.includes('Opening Weekend USA')) {
+        var [earnings, date] = value;
+        value = {
+          "Opening Weekend USA Earnings": earnings,
+          "Opening Weekend USA Date": date
+        }
+      }
+      imdb[key] = value
+    }
+  }
+  // var [key, value] = temp.split(/:\s{0,}/);
+  // imdb[key] = key === "Plot" ? [data.split(' See full summary')[0]] : value.split(", ");
+})
 
-console.log(rows);
+
+//Capture Source
+
+capturedData = window.location.href.match(/https:\/\/www.imdb.com\/title\/tt+\d{1,}/)[0];
+imdb['URL'] = [capturedData];
+
+
+console.log(imdb);
