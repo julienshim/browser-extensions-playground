bf813b84393016828cab72a1bfdb16956e842a91
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-09-04 21:10:34 -0700

    rename

--- Chat Analysis/comment.js
+++
@@ -1,6 +0,0 @@
-arr = [...document.querySelectorAll('ytd-comment-thread-renderer')].map(x => {
-  if(!x.innerText.includes('고정함')) {
-      var [user, time, comment, likes, reply, replies] = x.innerText.split(/\n/);
-      return {user, comment}
-  }
-}).filter(x => x);
